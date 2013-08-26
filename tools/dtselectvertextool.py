4983d25795e059679673b56200c153086ad49283
Bernhard Ströbl <bernhard.stroebl@jena.de>
2013-08-26 11:47:17 +0200

    Pass amount of desired vertices to selectvertextool
    The tool's return value is an array with arrays now

--- tools/dtselectvertextool.py
+++ tools/dtselectvertextool.py
@@ -12,6 +12,7 @@ subsumes different tools neded during digitizing sessions
 
 * begin                : 2013-08-11
 * copyright            : (C) 2013 by Angelos Tzotsos
+* revision             : Bernhard Ströbl (2013-08-25)
 * email                : tzotsos@gmail.com
 
 This program is free software; you can redistribute it and/or modify
@@ -20,25 +21,28 @@ the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 """
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtCore,  QtGui
 from qgis.core import *
 from qgis.gui import *
 
 # Vertex Select Tool class
 class DtSelectVertexTool(QgsMapTool):
-    def __init__(self, canvas):
+    '''select and mark numVertices vertices in the active layer'''
+    vertexFound = QtCore.pyqtSignal(list)
+
+    def __init__(self, canvas,  numVertices = 1):
         QgsMapTool.__init__(self,canvas)
         self.canvas=canvas
+        # desired number of marked vertex until signal
+        self.numVertices = numVertices
         # number of marked vertex
         self.count = 0
-        # 2 markers and vertex points
-        self.m1 = None #QgsVertexMarker(self.canvas)
-        self.m2 = None #QgsVertexMarker(self.canvas)
-        self.p1 = QgsPoint()
-        self.p2 = QgsPoint()
+        # arrays to hold markers and vertex points
+        self.markers = []
+        self.points = []
+        self.fids = []
         #custom cursor
-        self.cursor = QCursor(QPixmap(["16 16 3 1",
+        self.cursor = QtGui.QCursor(QtGui.QPixmap(["16 16 3 1",
                                         "      c None",
                                         ".     c #FF0000",
                                         "+     c #FFFFFF",
@@ -58,67 +62,62 @@ class DtSelectVertexTool(QgsMapTool):
                                         "    ++.....+    ",
                                         "      ++.++     ",
                                         "       +.+      "]))
-                                  
- 
-    
- 
+
     def canvasPressEvent(self,event):
         pass
-  
+
     def canvasMoveEvent(self,event):
         pass
-  
+
     def canvasReleaseEvent(self,event):
-        #Get the click
-        x = event.pos().x()
-        y = event.pos().y()
-        
-        layer = self.canvas.currentLayer()
-        
-        if layer <> None:
-            #the clicked point is our starting point
-            startingPoint = QPoint(x,y)
-                
-            #we need a snapper, so we use the MapCanvas snapper   
-            snapper = QgsMapCanvasSnapper(self.canvas)
+        if self.count < self.numVertices: #not yet enough
+            #Get the click
+            x = event.pos().x()
+            y = event.pos().y()
 
-            #we snap to the current layer (we don't have exclude points and use the tolerances from the qgis properties)
-            (retval,result) = snapper.snapToCurrentLayer (startingPoint,QgsSnapper.SnapToVertex)
+            layer = self.canvas.currentLayer()
 
-            if result == []:
-                #warn about missing snapping tolerance if appropriate
-                #self.showSettingsWarning()
-                pass
-            
-            if result <> []:
-                #mark the vertex 
-                if self.count == 0:
-                    #self.p1 = QgsPoint()
-                    self.p1.setX( result[0].snappedVertex.x() )  
-                    self.p1.setY( result[0].snappedVertex.y() )  
-                    self.m1 = QgsVertexMarker(self.canvas)
-                    self.m1.setIconType(1)
-                    self.m1.setColor(QColor(255,0,0))
-                    self.m1.setIconSize(12)
-                    self.m1.setPenWidth (3)            
-                    self.m1.setCenter(self.p1)
-                    self.count = self.count + 1
-                elif self.count == 1:
-                    self.p2.setX( result[0].snappedVertex.x() )  
-                    self.p2.setY( result[0].snappedVertex.y() )  
-                    self.m2 = QgsVertexMarker(self.canvas)
-                    self.m2.setIconType(1)
-                    self.m2.setColor(QColor(0,0,255))
-                    self.m2.setIconSize(12)
-                    self.m2.setPenWidth (3)            
-                    self.m2.setCenter(self.p2)
-                    self.count = self.count + 1
-                    #tell the world about the vertex and the marker           
-                    self.emit(SIGNAL("vertexFound(PyQt_PyObject)"), [self.p1, self.p2,  self.m1,  self.m2])            
-                else:
-                    #QMessageBox.information(None,  "Cancel",  str(self.m1))
+            if layer <> None:
+                #the clicked point is our starting point
+                startingPoint = QtCore.QPoint(x,y)
+
+                #we need a snapper, so we use the MapCanvas snapper
+                snapper = QgsMapCanvasSnapper(self.canvas)
+
+                #we snap to the current layer (we don't have exclude points and use the tolerances from the qgis properties)
+                (retval,result) = snapper.snapToCurrentLayer (startingPoint,QgsSnapper.SnapToVertex)
+
+                if result == []:
+                    #warn about missing snapping tolerance if appropriate
+                    #self.showSettingsWarning()
                     pass
-	      
+
+                if result <> []:
+                    #mark the vertex
+                    p = QgsPoint()
+                    p.setX( result[0].snappedVertex.x() )
+                    p.setY( result[0].snappedVertex.y() )
+                    m = QgsVertexMarker(self.canvas)
+                    m.setIconType(1)
+
+                    if self.count == 0:
+                        m.setColor(QtGui.QColor(255,0,0))
+                    else:
+                        m.setColor(QtGui.QColor(0, 0, 255))
+
+                    m.setIconSize(12)
+                    m.setPenWidth (3)
+                    m.setCenter(p)
+                    self.points.append(p)
+                    self.markers.append(m)
+                    fid = result[0].snappedAtGeometry # QgsFeatureId of the snapped geometry
+                    self.fids.append(fid)
+                    self.count += 1
+
+                    if self.count == self.numVertices:
+                        self.vertexFound.emit([self.points,  self.markers,  self.fids])
+                        #self.emit(SIGNAL("vertexFound(PyQt_PyObject)"), [self.points,  self.markers])
+
     def showSettingsWarning(self):
         m = QgsMessageViewer()
         m.setWindowTitle("Snap tolerance")
@@ -127,32 +126,27 @@ class DtSelectVertexTool(QgsMapTool):
         m.setCheckBoxQSettingsLabel(settingsLabel)
         m.setMessageAsHtml( "<p>Could not snap vertex.</p><p>Have you set the tolerance in Settings > Project Properties > General?</p>")
         m.showMessage()
-      
+
     def activate(self):
         self.canvas.setCursor(self.cursor)
-    
+
     def deactivate(self):
-        self.canvas.scene().removeItem(self.m1)
-        self.canvas.scene().removeItem(self.m2)   
-        self.m1 = None #QgsVertexMarker(self.canvas)
-        self.m2 = None #QgsVertexMarker(self.canvas)   
-        self.count = 0 
-        pass
+        self.clear()
 
     def clear(self):
-        self.canvas.scene().removeItem(self.m1)
-        self.canvas.scene().removeItem(self.m2)   
-        self.m1 = None #QgsVertexMarker(self.canvas)
-        self.m2 = None #QgsVertexMarker(self.canvas)   
+        for m in self.markers:
+            self.canvas.scene().removeItem(m)
+
+        self.markers = []
+        self.points = []
+        self.fids = []
         self.count = 0
-        p1 = None
-        p2 = None
 
     def isZoomTool(self):
         return False
-    
+
     def isTransient(self):
         return False
-      
+
     def isEditTool(self):
         return True
