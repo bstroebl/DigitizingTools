9d677ad4887ad05283715e1c573c897252aec1ef
Bernhard Ströbl <bernhard.stroebl@jena.de>
2013-10-16 16:09:42 +0200

    Create abstract classes for single and dual tools

---
+++ tools/dttools.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+dttools
+`````````````
+"""
+"""
+Part of DigitizingTools, a QGIS plugin that
+subsumes different tools neded during digitizing sessions
+
+* begin                : 2013-02-25
+* copyright          : (C) 2013 by Bernhard Ströbl
+* email                : bernhard.stroebl@jena.de
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+from PyQt4 import QtGui
+from qgis.core import *
+from qgis.gui import *
+from dtselectfeaturetool import DtSelectFeatureTool
+
+class DtTool():
+    '''Abstract class for a checkable tool
+    icon [QtGui.QIcon]
+    tooltip [str]
+    geometryTypes [array:integer] 0=point, 1=line, 2=polygon'''
+
+    def __init__(self, iface,  toolBar,  icon,  tooltip,  geometryTypes = [0, 1, 2]):
+        # Save reference to the QGIS interface
+        self.iface = iface
+        self.canvas = self.iface.mapCanvas()
+        self.act = QtGui.QAction(icon, tooltip,  self.iface.mainWindow())
+        self.act.setCheckable(True)
+        self.canvas.mapToolSet.connect(self.deactivate)
+        self.act.triggered.connect(self.run)
+        self.iface.currentLayerChanged.connect(self.enable)
+        toolBar.addAction(self.act)
+        self.geometryTypes = geometryTypes
+        self.enable()
+
+    def run(self):
+        raise NotImplementedError("Should have implemented run")
+
+    def enable(self):
+        '''Enables/disables the corresponding button.'''
+        # Disable the Button by default
+        self.act.setEnabled(False)
+        layer = self.iface.activeLayer()
+
+        if layer <> None:
+            #Only for vector layers.
+            if layer.type() == QgsMapLayer.VectorLayer:
+                # only for layers of
+                if self.geometryTypes.count(layer.geometryType()) == 1:
+                    #check if this layer'S geometry type is within the list of allowed types
+                    self.act.setEnabled(layer.isEditable())
+                    try:
+                        layer.editingStarted.disconnect(self.enable) # disconnect, will be reconnected
+                    except:
+                        pass
+                    try:
+                        layer.editingStopped.disconnect(self.enable) # when it becomes active layer again
+                    except:
+                        pass
+                    layer.editingStarted.connect(self.enable)
+                    layer.editingStopped.connect(self.enable)
+
+class DtDualTool():
+    '''Abstract class for a tool with interactive and batch mode
+    icon [QtGui.QIcon] for interactive mode
+    tooltip [str] for interactive mode
+    iconBatch [QtGui.QIcon] for batch mode
+    tooltipBatch [str] for batch mode
+    geometryTypes [array:integer] 0=point, 1=line, 2=polygon'''
+
+    def __init__(self, iface,  toolBar,  icon,  tooltip,  iconBatch,  tooltipBatch,  geometryTypes = [1, 2, 3]):
+        # Save reference to the QGIS interface
+        self.iface = iface
+        self.iface.currentLayerChanged.connect(self.enable)
+        self.canvas = self.iface.mapCanvas()
+        self.tool = DtSelectFeatureTool(self.canvas)
+        self.canvas.mapToolSet.connect(self.toolChanged)
+        #create button
+        self.button = QtGui.QToolButton(toolBar)
+        self.button.clicked.connect(self.run)
+        self.button.toggled.connect(self.hasBeenToggled)
+        #create menu
+        self.menu = QtGui.QMenu(toolBar)
+        self.menu.triggered.connect(self.menuTriggered)
+        self.button.setMenu(self.menu)
+        self.button.setPopupMode(QtGui.QToolButton.MenuButtonPopup)
+        # create actions
+        self.act = QtGui.QAction(icon, tooltip,  self.iface.mainWindow())
+        self.act_batch = QtGui.QAction(iconBatch, tooltipBatch,  self.iface.mainWindow())
+        self.menu.addAction(self.act)
+        self.menu.addAction(self.act_batch)
+        # set the interactive action as default action, user needs to click the button to activate it
+        self.button.setIcon(self.act.icon())
+        self.button.setCheckable(True)
+        self.batchMode = False
+        # add button to toolBar
+        toolBar.addWidget(self.button)
+        self.geometryTypes = geometryTypes
+        # run the enable slot
+        self.enable()
+
+    def menuTriggered(self,  thisAction):
+        if thisAction == self.act:
+            self.batchMode = False
+            self.button.setCheckable(True)
+            if not self.button.isChecked():
+                self.button.toggle()
+        else:
+            self.batchMode = True
+            if self.button.isCheckable():
+                if self.button.isChecked():
+                    self.button.toggle()
+                self.button.setCheckable(False)
+
+            self.run(False)
+
+        self.button.setIcon(thisAction.icon())
+
+    def hasBeenToggled(self,  isChecked):
+        try:
+            self.tool.featureSelected.disconnect(self.featureSelectedSlot)
+            # disconnect if it was already connected, so slot gets called only once!
+        except:
+            pass
+
+        if isChecked:
+            self.canvas.setMapTool(self.tool)
+            self.tool.featureSelected.connect(self.featureSelectedSlot)
+        else:
+            self.canvas.unsetMapTool(self.tool)
+
+    def toolChanged(self,  thisTool):
+        if thisTool != self.tool:
+            self.deactivate()
+
+    def deactivate(self):
+        if self.button.isChecked():
+            self.button.toggle()
+
+    def featureSelectedSlot(self,  fids):
+        if len(fids) >0:
+            self.process()
+
+    def run(self,  isChecked):
+        if self.batchMode:
+            layer = self.iface.activeLayer()
+
+            if layer.selectedFeatureCount() > 0:
+                self.process()
+        else:
+            if not isChecked:
+                self.button.toggle()
+
+    def process(self):
+        raise NotImplementedError("Should have implemented process")
+
+    def enable(self):
+       # Disable the Button by default
+        self.button.setEnabled(False)
+        layer = self.iface.activeLayer()
+
+        if layer <> None:
+            #Only for vector layers.
+            if layer.type() == QgsMapLayer.VectorLayer:
+                # only for certain layers
+                if self.geometryTypes.count(layer.geometryType()) == 1:
+                    if not layer.isEditable():
+                        self.deactivate()
+
+                    self.button.setEnabled(layer.isEditable())
+
+                    try:
+                        layer.editingStarted.disconnect(self.enable) # disconnect, will be reconnected
+                    except:
+                        pass
+                    try:
+                        layer.editingStopped.disconnect(self.enable) # when it becomes active layer again
+                    except:
+                        pass
+                    layer.editingStarted.connect(self.enable)
+                    layer.editingStopped.connect(self.enable)
+                else:
+                    self.deactivate()
