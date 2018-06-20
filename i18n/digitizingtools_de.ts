4e2f5eb5b04649ab1ced8206cf7719f2a337ad06
Bernhard Ströbl <bernhard.stroebl@jena.de>
2018-06-20 10:46:06 +0200

    [i18n] Update German translation

--- i18n/digitizingtools_de.ts
+++ i18n/digitizingtools_de.ts
@@ -4,32 +4,32 @@
 <context>
     <name>DigitizingTools</name>
     <message>
-        <location filename="../digitizingtools.py" line="79"/>
+        <location filename="../digitizingtools.py" line="84"/>
         <source>&amp;DigitizingTools</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../digitizingtools.py" line="80"/>
+        <location filename="../digitizingtools.py" line="85"/>
         <source>Help</source>
         <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="../digitizingtools.py" line="81"/>
+        <location filename="../digitizingtools.py" line="86"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../digitizingtools.py" line="83"/>
+        <location filename="../digitizingtools.py" line="88"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../tools/dttools.py" line="223"/>
+        <location filename="../tools/dttools.py" line="249"/>
         <source>is disabled because layer CRS and project CRS do not match!</source>
         <translation>wird deaktiviert weil das KBS des Layers und das KBS des Projektes nicht übereinstimmen!</translation>
     </message>
     <message>
-        <location filename="../tools/dtmerge.py" line="70"/>
+        <location filename="../tools/dtmerge.py" line="92"/>
         <source>The geometry type of the result is not valid in this layer!</source>
         <translation>Der Geometrietyp des Ergebnissses ist in diesem Layer ungültig!</translation>
     </message>
@@ -149,7 +149,7 @@
         <translation>Mit einem Polygon eines anderen Layers ausstanzen</translation>
     </message>
     <message>
-        <location filename="../tools/dtcutter.py" line="43"/>
+        <location filename="../tools/dtcutter.py" line="44"/>
         <source>Please provide a polygon layer to cut with.</source>
         <translation>Bitte einen Polygonlayer zum Ausstanzen bereitstellen.</translation>
     </message>
@@ -159,47 +159,47 @@
         <translation>Ausstanzlayer</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="138"/>
+        <location filename="../tools/dtutils.py" line="130"/>
         <source>No Selection in layer</source>
         <translation>Keine Auswahl in Layer</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="139"/>
+        <location filename="../tools/dtutils.py" line="131"/>
         <source>Use all features for process?</source>
         <translation>Alle Objekte zum Prozessieren benutzen?</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="144"/>
+        <location filename="../tools/dtutils.py" line="136"/>
         <source>There are </source>
         <translation>Es gibt </translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="146"/>
+        <location filename="../tools/dtutils.py" line="138"/>
         <source> features selected in layer </source>
         <translation> gewählte Objekte im Layer </translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="151"/>
+        <location filename="../tools/dtutils.py" line="143"/>
         <source>There are invalid geometries in layer </source>
         <translation>Es gibt ungültige Geometrien im Layer </translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="156"/>
+        <location filename="../tools/dtutils.py" line="148"/>
         <source>Snap Tolerance</source>
         <translation>Fangtoleranz</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="157"/>
+        <location filename="../tools/dtutils.py" line="149"/>
         <source>Could not snap vertex</source>
         <translation>Konnte keinen Stützpunkt fangen</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="159"/>
+        <location filename="../tools/dtutils.py" line="151"/>
         <source>Have you set the tolerance in Settings &gt; Snapping Options?</source>
         <translation>Haben Sie die Toleranz in Einstellungen &gt; Fangoptionen eingetragen?</translation>
     </message>
     <message>
-        <location filename="../tools/dtutils.py" line="171"/>
+        <location filename="../tools/dtutils.py" line="163"/>
         <source>Error occured during</source>
         <translation>Während dieses Prozesses trat ein Fehler auf</translation>
     </message>
@@ -209,107 +209,107 @@
         <translation>Stanze</translation>
     </message>
     <message>
-        <location filename="../tools/dtcutter.py" line="123"/>
+        <location filename="../tools/dtcutter.py" line="159"/>
         <source>A feature would be completely removed by cutting. Delete this feature&apos;s dataset altogether?</source>
         <translation>Durch das Ausstanzen würde ein Objekt komplett entfernt werden. Soll der Datensatz dieses Objektes gelöscht werden?</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="38"/>
+        <location filename="../tools/dtsplitter.py" line="39"/>
         <source>Splitter</source>
         <translation>Aufteilen</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="39"/>
+        <location filename="../tools/dtsplitter.py" line="40"/>
         <source>splitter layer</source>
         <translation>Aufteilungslayer</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="42"/>
+        <location filename="../tools/dtsplitter.py" line="43"/>
         <source>Please provide a line layer to split with.</source>
         <translation>Bitte stellen Sie einen Linienlayer zum Aufteilen bereit.</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitfeature.py" line="92"/>
+        <location filename="../tools/dtsplitfeature.py" line="94"/>
         <source>splitting of feature</source>
         <translation>Objekt aufteilen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="48"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="45"/>
         <source>Move polygon node (along a side) to achieve target area</source>
         <translation>Stützpunkt (entlang einer Seite) so verschieben, dass eine Zielfläche erreicht wird</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="84"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="81"/>
         <source>Move polygon node by area</source>
         <translation>Stützpunkt durch Festlegen einer Zielfläche verschieben</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="85"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="82"/>
         <source>Please select one polygon to edit.</source>
         <translation>Bitte wählen Sie das Polygon aus, das bearbeitet werden soll.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="87"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="84"/>
         <source>Please select only one polygon to edit.</source>
         <translation>Bitte wählen Sie nur ein Polygon aus.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="146"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="143"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="138"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="135"/>
         <source>Target Area not valid.</source>
         <translation>Die Zielfläche ist ungültig.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="141"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="138"/>
         <source>Not enough vertices selected.</source>
         <translation>Sie haben nicht genug Knoten ausgewählt.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="148"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="145"/>
         <source>Selected vertices should be consecutive on the selected polygon.</source>
         <translation>Die ausgewählten Knoten müssen einander benachbart sein.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="163"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="160"/>
         <source>Vertices not on the selected polygon.</source>
         <translation>Die ausgewählten Knoten gehören nicht zum ausgewählten Polygon.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebydistance.py" line="49"/>
+        <location filename="../tools/dtmovesidebydistance.py" line="47"/>
         <source>Parallel move of polygon side to given distance</source>
         <translation>Paralleles Verschieben einer Seite durch Festlegen einer Entfernung</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebydistance.py" line="83"/>
+        <location filename="../tools/dtmovesidebydistance.py" line="81"/>
         <source>Move polygon side by distance</source>
         <translation>Verschieben einer Seite durch Festlegen einer Entfernung</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebydistance.py" line="138"/>
+        <location filename="../tools/dtmovesidebydistance.py" line="136"/>
         <source>Target Distance not valid.</source>
         <translation>Die Entfernung ist ungültig.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="142"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="139"/>
         <source>Polygon side not selected.</source>
         <translation>Sie haben keine Polygonseite ausgewählt.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="146"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="143"/>
         <source>Selected segment should be on the selected polygon.</source>
         <translation>Das ausgewählte Segment muß auf dem ausgewählten Polygon liegen.</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="49"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="46"/>
         <source>Parallel move of polygon side to target area</source>
         <translation>Paralleles Verschieben einer Seite durch Festlegen einer Zielfläche</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="82"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="79"/>
         <source>Move polygon side by area</source>
         <translation>Verschieben einer Seite durch Festlegen einer Zielfläche</translation>
     </message>
@@ -364,32 +364,32 @@
         <translation>Alle Risse in den ausgewählten Objekten mit neuen Objekten fluten</translation>
     </message>
     <message>
-        <location filename="../tools/dtmedianline.py" line="42"/>
+        <location filename="../tools/dtmedianline.py" line="45"/>
         <source>Digitize median line between adjacent polygons</source>
         <translation>Mittellinie zwischen zwei benachbarten Polygonen erzeugen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmedianline.py" line="85"/>
+        <location filename="../tools/dtmedianline.py" line="86"/>
         <source>Digitize median line</source>
         <translation>Mittellinie erzeugen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmedianline.py" line="93"/>
+        <location filename="../tools/dtmedianline.py" line="94"/>
         <source>Please clear selection.</source>
         <translation>Bitte löschen Sie die Auswahl.</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="53"/>
+        <location filename="../tools/dtsplitter.py" line="54"/>
         <source> Please select only one feature to split with.</source>
         <translation> Bitte wählen Sie nur ein Objekt aus, mit dem aufgeteilt werden soll.</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="57"/>
+        <location filename="../tools/dtsplitter.py" line="58"/>
         <source> Please select the features to be splitted.</source>
         <translation> Bitte wählen Sie die Objekte aus, die aufgeteilt werden sollen.</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitter.py" line="29"/>
+        <location filename="../tools/dtsplitter.py" line="30"/>
         <source>Split selected features with selected line from another layer</source>
         <translation>Ausgewählte Objekte mit ausgewählter Linie eines anderen Layers aufteilen</translation>
     </message>
@@ -429,19 +429,14 @@
         <translation>Teil abspalten und als neues Objekt einfügen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmerge.py" line="30"/>
+        <location filename="../tools/dtmerge.py" line="33"/>
         <source>Merge selected features</source>
         <translation>Gewählte Objekte verschmelzen</translation>
     </message>
     <message>
-        <location filename="../tools/dtmerge.py" line="39"/>
+        <location filename="../tools/dtmerge.py" line="42"/>
         <source>Merge</source>
         <translation>Verschmelzen</translation>
-    </message>
-    <message>
-        <location filename="../tools/dtmerge.py" line="51"/>
-        <source>Choose which feature should remain</source>
-        <translation>Wählen Sie das Objekt, das übrigbleiben soll</translation>
     </message>
     <message>
         <location filename="../tools/dtexchangegeometry.py" line="29"/>
@@ -449,30 +444,35 @@
         <translation>Attribute zwischen gewählten Objekten vertauschen</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitfeature.py" line="46"/>
+        <location filename="../tools/dtsplitfeature.py" line="48"/>
         <source>Split Features</source>
         <translation>Objekte zerteilen</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitfeature.py" line="116"/>
+        <location filename="../tools/dtsplitfeature.py" line="118"/>
         <source>Split Multipart Feature</source>
         <translation>Multiobjekt zerteilen</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitfeature.py" line="116"/>
+        <location filename="../tools/dtsplitfeature.py" line="118"/>
         <source>Create new feature from this part?</source>
         <translation>Diesen Teil als neues Objekt erzeugen?</translation>
+    </message>
+    <message>
+        <location filename="../tools/dtToolsDialog.py" line="42"/>
+        <source>Choose which already existing feature should remain</source>
+        <translation>Wählen Sie das bereits existierende Objekt, das übrigbleiben soll</translation>
     </message>
 </context>
 <context>
     <name>dtAbout</name>
     <message>
-        <location filename="../dtDialog.py" line="35"/>
+        <location filename="../dtDialog.py" line="36"/>
         <source>Subsumes different tools useful during digitizing sessions</source>
         <translation>Fasst verschiedene nützliche Digitalisierwerkzeuge zusammen</translation>
     </message>
     <message>
-        <location filename="../dtDialog.py" line="37"/>
+        <location filename="../dtDialog.py" line="38"/>
         <source>List of Contributors:</source>
         <translation>Folgende Personen haben beigetragen:</translation>
     </message>
@@ -480,7 +480,7 @@
 <context>
     <name>dtutils</name>
     <message>
-        <location filename="../tools/dtutils.py" line="124"/>
+        <location filename="../tools/dtutils.py" line="119"/>
         <source>Choose Layer</source>
         <translation>Layer wählen</translation>
     </message>
@@ -488,27 +488,27 @@
 <context>
     <name>editcommand</name>
     <message>
-        <location filename="../tools/dtsplitter.py" line="67"/>
+        <location filename="../tools/dtsplitter.py" line="66"/>
         <source>Split features</source>
         <translation>Geometrien aufgeteilt</translation>
     </message>
     <message>
-        <location filename="../tools/dtcutter.py" line="85"/>
+        <location filename="../tools/dtcutter.py" line="91"/>
         <source>Cut Features</source>
         <translation>Objekte ausgestanzt</translation>
     </message>
     <message>
-        <location filename="../tools/dtcutter.py" line="154"/>
+        <location filename="../tools/dtcutter.py" line="190"/>
         <source>Delete Features</source>
         <translation>Objekte gelöscht</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovenodebyarea.py" line="153"/>
+        <location filename="../tools/dtmovenodebyarea.py" line="150"/>
         <source>Move Node By Area</source>
         <translation>Stützpunkt durch Festlegen einer Zielfläche verschoben</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebydistance.py" line="151"/>
+        <location filename="../tools/dtmovesidebydistance.py" line="149"/>
         <source>Move Side By Distance</source>
         <translation>Seite durch Festlegen einer Entfernung verschoben</translation>
     </message>
@@ -523,7 +523,7 @@
         <translation>Ringe gefüllt</translation>
     </message>
     <message>
-        <location filename="../tools/dtmovesidebyarea.py" line="157"/>
+        <location filename="../tools/dtmovesidebyarea.py" line="154"/>
         <source>Move Side By Area</source>
         <translation>Seite durch Festlegen einer Zielfläche verschoben</translation>
     </message>
@@ -558,7 +558,7 @@
         <translation>Teil extrahiert</translation>
     </message>
     <message>
-        <location filename="../tools/dtclipper.py" line="93"/>
+        <location filename="../tools/dtclipper.py" line="91"/>
         <source>Clip Features</source>
         <translation>Objekte ausgeschnitten</translation>
     </message>
@@ -568,12 +568,12 @@
         <translation>Attribute vertauscht</translation>
     </message>
     <message>
-        <location filename="../tools/dtmerge.py" line="57"/>
+        <location filename="../tools/dtmerge.py" line="71"/>
         <source>Merge Features</source>
         <translation>Objekte verschmolzen</translation>
     </message>
     <message>
-        <location filename="../tools/dtsplitfeature.py" line="66"/>
+        <location filename="../tools/dtsplitfeature.py" line="68"/>
         <source>Features split</source>
         <translation>Objekte geteilt</translation>
     </message>
